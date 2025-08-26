44b604734aff0d8197c244d52e86e95af2ecf205
andre <andreyleontev199@gmail.com>
2025-08-26 16:17:53 +0500

    1

--- BankClient/obj/Debug/net8.0/BankClient.AssemblyInfo.cs
+++ BankClient/obj/Debug/net8.0/BankClient.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankClient")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bfa523854048b1c57a6da9d3efb3041a148aab3")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankClient")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankClient")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
